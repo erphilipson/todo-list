3cf327ba6639926c95aea6cf7becf03a9ab4e487
Ethan Philipson <erphilipson@gmail.com>
2017-06-28 14:46:27 -0400

    Add sequelize functionality and checkboxes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const models = require('./models');
 const mustacheExpress = require('mustache-express');
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,19 +11,50 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
+// const task = models.Task.build({
+//   name: 'clean',
+//   priority: 5
+// });
+
 let todo = [];
 let html ='';
+
+// task.save().then(function(newTask){
+//   console.log(newTask.id);
+// })
+
+// models.Task.findAll().then(function(tasks){
+//   for (i in tasks){
+//   console.log(tasks[i].name);
+// }
+// })
+
+// models.Task.findOne({where: {id: 1} }).then(function(task){
+//   console.log(task.name);
+// })
+
 
 app.get ('/', function (req, res){
   res.render ('index', {todo: todo});
 });
 
 app.post ('/', function (req, res){
+
   let item = {
     'task': req.body.task,
     'checked': ''
   };
+
   todo.push(item);
+
+  let task = models.Task.build({
+    name: req.body.task,
+    priority: req.body.priority
+  });
+
+  task.save().then(function(newTask){
+  })
+
   res.render('index', {todo: todo});
 })
 
